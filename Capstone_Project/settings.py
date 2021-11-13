73da4d7d4ec07b1fa78a540e3b79dee7645976ca
motatoes <moe.habib9@gmail.com>
2021-11-13 23:53:01 +0000

    add reqs and use sqlite

--- Capstone_Project/settings.py
+++ Capstone_Project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Capstone_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'shahram',
         'USER': 'shahram',
         'PASSWORD': '',
